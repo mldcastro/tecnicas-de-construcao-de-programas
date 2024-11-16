a1c805f2201a95a3d751d60575f05d91e8e91856
Matheus Luís de Castro <matheuslc36.mc@gmail.com>
2024-11-15 23:20:23 -0300

    Implement basic GUI

--- src/user_input.py
+++ src/user_input.py
@@ -1,37 +1,74 @@
-from .input_error import InputException
+from typing import Optional
+from PyQt6 import QtWidgets
+
+from .input_error import InputException, InputErrorBox
 
 
-class UserInputBox(...):
-    def __init__(self, *args, **kwargs) -> None: ...
+class UserInputWidget(QtWidgets.QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._layout = QtWidgets.QGridLayout()
+        self.setLayout(self._layout)
+
+        self._input = QtWidgets.QTextEdit()
+        self._input.setFixedHeight(50)
+        self._input.setFixedWidth(300)
+        self._input.setPlaceholderText("Type here")
+
+        self._ok_button = QtWidgets.QPushButton("OK")
+        self._ok_button.setFixedWidth(50)
+        self._ok_button.clicked.connect(self._on_ok_pressed)
+
+        self._error_box = InputErrorBox()
+        self._error_box.hide()
+
+        self._layout.addWidget(self._input)
+        self._layout.addWidget(self._ok_button)
+        self._layout.addWidget(self._error_box)
+
+        self._value: Optional[str] = None
+        self._is_valid: Optional[bool] = None
+        self._is_blocked = False
 
     @property
-    def value(self) -> str: ...
-
-    # Texto contido dentro da caixa de texto.
-
-    @property
-    def is_blocked(self) -> bool: ...
-
-    # Nome auto explicativo.
+    def value(self) -> Optional[str]:
+        return self._value
 
     @property
-    def is_valid(self) -> bool: ...
+    def is_blocked(self) -> bool:
+        return self._is_blocked
 
-    # O input é válido ou não.
+    @property
+    def is_valid(self) -> Optional[bool]:
+        return self._is_valid
 
-    def _validate_input(self) -> None:
-        # Nome auto explicativo.
-        ...
-        raise InputException("Invalid input")
+    def block(self) -> None:
+        self._input.setDisabled(True)
+        self._ok_button.setDisabled(True)
+        self._is_blocked = True
 
-    def _on_ok_pressed(self) -> None: ...
+    def unblock(self) -> None:
+        self._input.setDisabled(False)
+        self._ok_button.setDisabled(False)
+        self._is_blocked = False
 
-    # Irá salvar a string, que fará a verificação do texto e envio para o mapeamento.
+    def _validate_input(self, input_str: str) -> None:
+        if not input_str:
+            raise InputException("Invalid input")
 
-    def block(self) -> None: ...
+    def _show_error(self, e: InputException) -> None:
+        self._error_box.set_text(str(e))
+        self._error_box.display()
 
-    # Nome auto explicativo.
+    def _on_ok_pressed(self) -> None:
+        try:
+            self._validate_input(self._input.toPlainText())
+            self._value = self._input.toPlainText()
+            self._is_valid = True
+            self._error_box.hide()
 
-    def unblock(self) -> None: ...
-
-    # Nome auto explicativo.
+        except InputException as e:
+            self._value = None
+            self._is_valid = False
+            self._show_error(e)
