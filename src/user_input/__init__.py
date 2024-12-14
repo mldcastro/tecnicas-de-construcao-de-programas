4a148def8c406226a14cafda582ca54b7c8a53d8
Matheus Luís de Castro <matheuslc36.mc@gmail.com>
2024-12-14 11:12:10 -0300

    [improvement] Reorganize user input related modules

---
+++ src/user_input/__init__.py
@@ -0,0 +1,6 @@
+from .user_input import UserInputWidget
+from .input_error import InputException, InputErrorBox
+from .input_validator import InputValidator
+
+
+__all__ = ["UserInputWidget", "InputException", "InputErrorBox", "InputValidator"]
